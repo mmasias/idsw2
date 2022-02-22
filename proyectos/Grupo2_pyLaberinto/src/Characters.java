01d497788e0678cad6bd82c6bcc9f6e803391382
ADRYEP <adr27yepez@gmail.com>
2022-02-22 17:14:20 +0100

    Character class created

---
+++ proyectos/Grupo2_pyLaberinto/src/Characters.java
@@ -0,0 +1,6 @@
+public class Characters {
+    public static boolean player = false;
+    public static boolean boat = false;
+    public static boolean horse = false;
+    public static boolean flyingCarpet = false;
+}
