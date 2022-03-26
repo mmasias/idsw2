a7286937d0959650e8f67016a979e32061753b06
Manuel Masías <mmasias@users.noreply.github.com>
2022-03-26 13:26:24 +0100

    Revert "Grupo 3"

--- BatallaSoftware/src/isii/other/Dimensions.java
+++
@@ -1,6 +0,0 @@
-package isii.other;
-
-public class Dimensions {
-	public static int heroineX = 1000, heroineY = 120, heroineWidth = 400, heroineHeight = 400;
-	public static int enemyX = 500, enemyY = 260, enemyWidth = 250, enemyHeight = 250;
-}
